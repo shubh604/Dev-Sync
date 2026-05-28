3787610bb12ab4ebd5f57efbd7031a180369b7cd
https://github.com/shubh604 <shubhdeep3110@gmail.com>
2026-05-28 22:45:54 +0530

    fix import issue

--- backend/config/transporter.js
+++ backend/config/transporter.js
@@ -1,7 +1,7 @@
-const Brevo = require("@getbrevo/brevo");
+const { TransactionalEmailsApi, TransactionalEmailsApiApiKeys } = require("@getbrevo/brevo");
 require("dotenv").config();
 
-const apiInstance = new Brevo.TransactionalEmailsApi();
-apiInstance.setApiKey(Brevo.TransactionalEmailsApiApiKeys.apiKey, process.env.BREVO_API_KEY);
+const apiInstance = new TransactionalEmailsApi();
+apiInstance.setApiKey(TransactionalEmailsApiApiKeys.apiKey, process.env.BREVO_API_KEY);
 
 module.exports = apiInstance;
