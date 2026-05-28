376529e1c8db15451bbd6bfa3dc3fc3b3156106a
https://github.com/shubh604 <shubhdeep3110@gmail.com>
2026-05-28 20:14:13 +0530

    Fix pc ui

--- backend/config/transporter.js
+++ backend/config/transporter.js
@@ -7,6 +7,8 @@ function transporter(){
     // transporter setup
     return nodeMailer.createTransport({
         host: process.env.MAIL_HOST,
+        port: 465,
+        secure: true,
         auth: {
             user: process.env.MAIL_USER,
             pass: process.env.MAIL_PASS,
